d0e2299146be415f54cf9a7ebaba9c02fb88bfa8
Vance-M <myveryvills@yahoo.com>
2021-01-13 14:56:43 -0800

    add grab Doms in app.js

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 // import functions and grab DOM elements
+const winRecord = document.getElementById('win-record');
+const loseRecord = document.getElementById('lose-record');
+const drawRecord = document.getElementById('draw-record');
+const computerChoice = document.getElementById('computer-choice');
+const currentResult = document.getElementById('current-result');
+const resetScores = document.getElementById('reset-scores');
+const gameTime = document.getElementById('game-time');
+
+// const currentResult = document.getElementById('');
 
 // initialize state
 
+let winRecordOrigin = '0';
+
+let loseRecordOrigin = '0';
+
+let drawRecordOrigin = '0';
+
+
+
 // set event listeners to update state and DOM
+
+const checkedInput = document.querySelector('input[type="radio"]:checked');
+
+gameTime.addEventListener('click', () =>{
+    console.log(1);
+    console.log(checkedInput);
+});
+
+
+
+
+
